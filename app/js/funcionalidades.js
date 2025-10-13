b21bbcd12d4291941e23bcce47c2b23bdbdd131d
mariamaAM-bit <mariamaamachado@gmail.com>
2025-10-13 15:54:28 -0300

    Add files via upload

---
+++ app/js/funcionalidades.js
@@ -0,0 +1,11 @@
+function aplicarPoupanca(valor, agencia, conta) {
+
+}
+
+function aplicarFundoImobiliario(valor, agencia, conta, prazo_mensal) {
+
+}
+
+function solicitarCredito(valor_tomado, agencia, conta, valor_a_devolver, prazo_mensal) {
+
+}
