0f9a9fa2eb469cacd96dbddb8d9b7ea14965f92d
mariamaAM-bit <mariamaamachado@gmail.com>
2025-10-14 16:38:26 -0300

    Update funcionalidades.js

--- app/js/funcionalidades.js
+++ app/js/funcionalidades.js
@@ -1,11 +1,12 @@
-function aplicarPoupanca(valor, agencia, conta) {
+function aplicarPoupanca(valor_inicial, valor_mensal, agencia, conta) {
 
 }
 
-function aplicarFundoImobiliario(valor, agencia, conta, prazo_mensal) {
+function aplicarFundoImobiliario(valor, agencia, conta, prazo_mensal, renda) {
 
 }
 
-function solicitarCredito(valor_tomado, agencia, conta, valor_a_devolver, prazo_mensal) {
+function solicitarCredito(valor_tomado, agencia, conta, valor_a_devolver, prazo_mensal, renda) {
+
 
 }
